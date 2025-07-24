463d5157fb93d2fdda5be686fba19c3cbff0a55c
@DavidLeoRojas <david.rojas@uptc.edu.co>
2025-07-24 11:55:54 -0500

    Agrego archivos JS de capas de datos para el mapa de Cartagena

---
+++ cartagena_barrios.js
@@ -0,0 +1,43 @@
+var cartagena_barrios = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 1,
+        "NOMBRE": "Centro Histórico"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [-75.5490, 10.4230],
+            [-75.5480, 10.4250],
+            [-75.5460, 10.4240],
+            [-75.5470, 10.4220],
+            [-75.5490, 10.4230]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 2,
+        "NOMBRE": "Getsemaní"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [-75.5495, 10.4200],
+            [-75.5485, 10.4220],
+            [-75.5465, 10.4210],
+            [-75.5475, 10.4190],
+            [-75.5495, 10.4200]
+          ]
+        ]
+      }
+    }
+  ]
+};
